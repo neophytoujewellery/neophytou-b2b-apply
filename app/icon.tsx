0e5464a3a7063a8af695d0d4e89f60b7c3f71180
rahman2220510189 <rahman22205101894@diu.edu.bd>
2026-09-22 15:29:55 +0600

    all done

---
+++ app/icon.tsx
@@ -0,0 +1,28 @@
+import { ImageResponse } from "next/og";
+
+export const size = { width: 32, height: 32 };
+export const contentType = "image/png";
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 24,
+          background: "#14100F",
+          color: "#B08D3F",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          fontWeight: 700,
+          borderRadius: 6,
+        }}
+      >
+        N
+      </div>
+    ),
+    { ...size }
+  );
+}
